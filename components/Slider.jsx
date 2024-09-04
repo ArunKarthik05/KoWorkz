7973d7b9fb1b537e55935f5d0b0c27914dcd0f6d
ArunKarthik05 <akkarthikk2002@gmail.com>
2024-09-04 23:24:21 +0530

    footer-mobile

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import styles from "../styles/modules/slider.module.scss";
+import Image from 'next/image';
 
 export default function Slider() {
     const slides = ["Day Pass","Private Offices","Office Suites","Assorted Rooms","Conference Halls","Dedicated Seats"];
 
   return (
     <div className={styles.slider}>
-        {slides.forEach((slide,i)=>{
-            <h2 key={i}>{slide}</h2>
+        {slides.map((slide,i)=>{
+            return(
+              <div className={styles.flexRow}>
+                <p key={i}>{slide}</p>
+                <Image src="/slider.png" alt='star' height="20" width="20"/>
+              </div>
+            )
         })}
     </div>
   )
